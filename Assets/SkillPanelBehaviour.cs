7e793c44b96a2a74c2914738f6c5f870db29b486
Yizhou Li <lyzpp2000@163.com>
2021-04-28 19:44:54 -0400

    Update-UIPanel-4.28.2021-Y.L

---
+++ Assets/SkillPanelBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillPanelBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		for (int i = 0; i < 5; i++)
+		{
+            transform.GetChild(i).GetChild(0).GetChild(0).GetComponent<Image>().sprite =
+                GameObject.FindGameObjectWithTag("Player")
+                .transform.GetChild(0).GetComponent<HeroAssets>().UIAssets[i];
+        }
+    }
+}
