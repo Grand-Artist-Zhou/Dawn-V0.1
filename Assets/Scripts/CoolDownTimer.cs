6bc6fac8b7b6a0b4f486cc5156a82b26ac0b4dd1
Yizhou Li <lyzpp2000@163.com>
2021-04-29 12:47:56 -0400

    fixes

---
+++ Assets/Scripts/CoolDownTimer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownTimer : MonoBehaviour
+{
+    public Text timeText;
+
+    private float timeRemaining;
+    private float cdCycle;
+    public bool timerIsRunning = false;
+    private string skillName;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+        skillName = GetComponentInParent<Transform>().GetComponentInParent<Transform>().name;
+		if (name == "SkillQ")
+		{
+            cdCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseAbility>().SkillQCD;
+            Debug.Log(cdCycle);
+		}
+		else if (name == "SkillW")
+		{
+            cdCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseAbility>().SkillWCD;
+        }
+        else if (name == "SkillE")
+        {
+            cdCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseAbility>().SkillECD;
+        }
+        else if (name == "SkillR")
+        {
+            cdCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseAbility>().SkillRCD;
+        }
+        else if (name == "SkillUlt")
+        {
+            cdCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseAbility>().SkillUltCD;
+        }
+        timeRemaining = cdCycle;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = cdCycle;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = seconds.ToString();
+    }
+}
---
+++ Assets/Scripts/HeroControl/BaseAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbility : MonoBehaviour
+{
+    public bool isSkillQPassive = false;
+    public bool isSkillWPassive = false;
+    public bool isSkillEPassive = false;
+    public bool isSkillRPassive = false;
+    public bool isSkillUltPassive = false;
+
+    public float SkillQCD;
+    public float SkillWCD;
+    public float SkillECD;
+    public float SkillRCD;
+    public float SkillUltCD;
+}
